1e3396a4f2d5c86cae280fef7cd6b4dda4a8162a
fengshi.yw <fengshi.yw@alibaba-inc.com>
2017-01-02 18:51:02 +0800

    fix route bug

--- node/app.js
+++ node/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const compress = require('compression')
@@ -7,12 +9,28 @@ const config = require('./config');
 
 const app = express();
 
+// use mid
 app.use(compress())
   .use(express.static(__dirname + '/public'))
   .use(bodyParser.json());
 
 const model = require('./model');
 model.setRestApi(app);
+
+
+// render
+app.engine('.html', require('ejs').__express);
+// 设置视图模板的默认后缀名为.html,避免了每次res.Render("xx.html")的尴尬
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'public'));
+
+app.get('/*', function (req, res) {
+  res.render('index', function (err, html) {
+    console.log('test')
+    res.send(html);
+  });
+});
+
 
 app.listen(config.port);
 console.log(`started @ ${config.port}`);
