d81cf4eb40c128a3a07c71dd422f1d90e90a6f69
changqi <llj@nbugs.com>
2017-01-02 15:45:22 +0800

    modify

--- front-end/src/layouts/MainLayout/MainLayout.jsx
+++ front-end/src/layouts/MainLayout/MainLayout.jsx
@@ -2,30 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import { Router, Route, IndexRoute, Link } from 'react-router';
 import styles from './MainLayout.less';
 
-const MainLayout = ({ children }) => {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.head}>
-        <h1>Todo App</h1>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.side}>
-          <h2>Filters:</h2>
-          <Link to="/">All</Link><br />
-          <Link to="/actived">Actived</Link><br />
-          <Link to="/completed">Completed</Link><br />
-          <Link to="/404">404</Link><br />
+class MainLayout extends Component {
+  constructor(props) {
+      super(props)
+  }
+
+  render() {
+    return (
+      <div className={styles.normal}>
+        <div className={styles.head}>
+          <h2>青叶</h2>
         </div>
+
         <div className={styles.main}>
-          {children}
+          {this.props.children}
+        </div>
+
+        <div className={styles.foot}>
+          Built with react, react-router, ant-tool, css-modules, antd...
         </div>
       </div>
-      <div className={styles.foot}>
-        Built with react, react-router, ant-tool, css-modules, antd...
-      </div>
-    </div>
-  );
-};
+    )
+  }
+}
 
 MainLayout.propTypes = {
   children: PropTypes.element.isRequired,
