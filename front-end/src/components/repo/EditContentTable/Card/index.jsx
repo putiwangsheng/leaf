ed42d5f87517bba98e4fc31190a1063f2d8fc98b
SimplyY <simplyyu@163.com>
2017-01-15 20:25:03 +0800

    finish TOC edit

--- front-end/src/components/repo/EditContentTable/Card/index.jsx
+++ front-end/src/components/repo/EditContentTable/Card/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 import { DragSource, DropTarget } from 'react-dnd';
+import styles from './index.less';
 
 const CARD_TYPE = 'CARD';
 
@@ -11,45 +11,92 @@ const cardSource = {
       id: props.id,
       index: props.index
     };
+  },
+
+  endDrag: function (props, monitor, component) {
+    if (!monitor.didDrop() || monitor.getDropResult() === undefined) {
+      return;
+    }
+
+    const { dragIndex, dropIndex, newRank, isFirstBlock } = monitor.getDropResult();
+
+    props.moveCard(dragIndex, dropIndex, newRank, isFirstBlock);
   }
 };
 
+let oldStyleInfo = {
+  style: '',
+  isFirstBlock: undefined
+}
+
+// 当位置和 style 未改变的时候，不用更新 style
+function needChangeStyle(oldInfo, newInfo) {
+  return oldInfo.style !== newInfo.style || oldInfo.isFirstBlock !== newInfo.isFirstBlock;
+}
+
 const cardTarget = {
+
+  // 需要显示 hover 处 drop 的效果，根据 isSameRank 来
   hover(props, monitor, component) {
     const dragIndex = monitor.getItem().index;
     const hoverIndex = props.index;
 
-    // Don't replace items with themselves
     if (dragIndex === hoverIndex) {
+      return
+    }
+
+    const hoverInfo = props.getHoverInfo(monitor, component, hoverIndex);
+    const styleInfo = {
+      isFirstBlock: hoverInfo.isFirstBlock
+    };
+
+    // 如果 isSameRank 或 有 dragCard 有子 card，style 应该为 line
+    if(hoverInfo.isSameRank || props.isHasChild(dragIndex)) {
+      styleInfo.style = 'line';
+    } else {
+      styleInfo.style = 'border';
+    }
+
+    // 如果不需要更新样式，则结束
+    if (!needChangeStyle(oldStyleInfo, styleInfo)) {
       return;
     }
 
-    // Determine rectangle on screen
-    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+    // 在需要更新样式的前体下，更新样式
+    if (styleInfo.style === 'line') {
+      console.log('line');
+    } else {
+      console.log('border')
+    }
 
-    // Dragging downwards
-    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+    oldStyleInfo = styleInfo;
+  },
+
+  // drop 时 move card， 需要改变 容器的 state，根据 newRank 来
+  drop(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const dropIndex = props.index;
+
+    // Don't replace items with themselves
+    if (dragIndex === dropIndex) {
       return;
     }
 
-    // Dragging upwards
-    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-      return;
+    // 1. get newRank
+    let { newRank, isFirstBlock } = props.getMoveInfo(monitor, component, dropIndex);
+    if (props.isHasChild(dragIndex)) {
+      newRank = 1;
     }
 
-    // Time to actually perform the action
-    props.moveCard(dragIndex, hoverIndex);
-
-    // Note: we're mutating the monitor item here!
-    monitor.getItem().index = hoverIndex;
+    return {
+      newRank,
+      isFirstBlock,
+      dragIndex,
+      dropIndex
+    }
   }
 };
+
 
 @DropTarget(CARD_TYPE, cardTarget, connect => ({
   connectDropTarget: connect.dropTarget()
@@ -58,7 +105,6 @@ const cardTarget = {
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging()
 }))
-
 class Card extends Component {
   constructor() {
     super();
@@ -69,7 +115,7 @@ class Card extends Component {
     const opacity = isDragging ? 0 : 1;
 
     return connectDragSource(connectDropTarget(
-      <div style={{ opacity }}>
+      <div className={styles.card} style={{ opacity }}>
         {text}
       </div>
     ));
--- front-end/src/components/repo/EditContentTable/index.jsx
+++ front-end/src/components/repo/EditContentTable/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
@@ -23,7 +25,7 @@ class EditContentTable extends Component {
       },
       {
         id: 2,
-        rank: 2,
+        rank: 1,
         title: 'js 大法好'
       },
       {
@@ -33,29 +35,114 @@ class EditContentTable extends Component {
       },
       {
         id: 4,
-        rank: 2,
+        rank: 1,
         title: '要学好英语'
       },
       {
         id: 5,
-        rank: 3,
+        rank: 1,
         title: '要学能阅读英语'
       }
     ];
   }
 
-  moveCard(dragIndex, hoverIndex) {
+  isHasChild(index) {
     const { cards } = this.state;
-    const dragCard = cards[dragIndex];
 
-    this.setState(update(this.state, {
-      cards: {
-        $splice: [
-          [dragIndex, 1],
-          [hoverIndex, 0, dragCard]
-        ]
+    // 边界条件
+    if (index >= cards.length - 1) {
+      return false;
+    }
+
+    return cards[index].rank === 1 && cards[index + 1].rank === 2;
+  }
+
+  getMoveInfo(monitor, component, hoverIndex) {
+    const moveInfo = {
+      newRank: 1,
+      isFirstBlock: false
+    };
+
+    // 1. get rectHeight
+    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+    const rectHeight = hoverBoundingRect.bottom - hoverBoundingRect.top;
+
+    // 2. get hoverClientHeight
+    const clientOffset = monitor.getClientOffset();
+    const hoverClientHeight = clientOffset.y - hoverBoundingRect.top;
+
+    // 3. get hoverRectRank and nextRectRank
+    const { cards } = this.state;
+    const hoverRectRank = cards[hoverIndex] && cards[hoverIndex].rank;
+    const nextRectRank = cards[hoverIndex + 1] && cards[hoverIndex + 1].rank;
+
+    // 4. judge newRank:三种情况分别讨论。整个判断逻辑请看 doc.md
+    if (hoverClientHeight < rectHeight/4) {
+      moveInfo.newRank = hoverRectRank;
+      moveInfo.isFirstBlock = true;
+    }
+    else if (hoverClientHeight > rectHeight/4 && hoverClientHeight < rectHeight * 3/4) {
+      moveInfo.newRank = 2;
+    }
+    else if (hoverClientHeight > rectHeight*3/4) {
+      // 不存在下一行的特殊情况。
+      if (cards[hoverIndex + 1] === undefined) {
+        moveInfo.newRank = 1;
+        return moveInfo;
       }
-    }));
+
+      moveInfo.newRank = hoverRectRank === 1 && nextRectRank === 2 ? 2 : 1;
+    }
+
+    return moveInfo;
+  }
+
+  // isSameRank 是跟 hover card 来比较
+  getHoverInfo(monitor, component, hoverIndex) {
+    const moveInfo = this.getMoveInfo(monitor, component, hoverIndex);
+
+    return {
+      ...moveInfo,
+      isSameRank: moveInfo.newRank === this.state.cards[hoverIndex].rank
+    };
+  }
+
+  moveCard(dragIndex, dropIndex, newRank, isFirstBlock) {
+    const { cards } = this.state;
+    const newCards = cards.slice();
+
+    const dragCard = cards[dragIndex];
+    const dropCard = cards[dropIndex];
+
+    dragCard.rank = newRank;
+
+    // 1. get moveCards from splice newCards
+    let moveCards;
+    // 移动 card, 分两种情况，dragCard 没子 card，有子 card
+    if (!this.isHasChild(dragIndex)) {
+      moveCards = newCards.splice(dragIndex, 1);
+    } else {
+      // 有子 card 需要把 子 card 全部移动
+      let childLen = cards.slice(dragIndex + 1).findIndex(
+        item => item.rank === 1
+      );
+      childLen = childLen === -1 ? cards.length - dragIndex : childLen;
+
+      moveCards = newCards.splice(dragIndex, childLen + 1);
+    }
+
+    // 2. get insertIndex
+    let insertIndex = newCards.findIndex(item => item.id === dropCard.id);
+    if (!isFirstBlock) {
+      insertIndex += 1;
+    }
+
+    // 3. insert moveCards
+    newCards.splice(insertIndex, 0, ...moveCards);
+
+    this.setState({
+      cards: newCards
+    });
   }
 
   render() {
@@ -78,7 +165,10 @@ class EditContentTable extends Component {
             index={i}
             id={item.id}
             text={item.title}
-            moveCard={this.moveCard.bind(this)} />
+            moveCard={this.moveCard.bind(this)}
+            getMoveInfo={this.getMoveInfo.bind(this)}
+            getHoverInfo={this.getHoverInfo.bind(this)}
+            isHasChild={this.isHasChild.bind(this)} />
         </div>
       );
     });
